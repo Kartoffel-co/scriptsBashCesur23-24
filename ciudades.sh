ef3a00ece2717824f06418eec072281fea9b9228
karto <karto@karto>
2023-10-03 09:15:08 +0200

    ciudades

--- ciudades.sh
+++ ciudades.sh
@@ -1,1 +1,16 @@
 #/bin/bash
+
+read -p "escribe el nombre de ciudades termina con fin " ciudad
+cont=0
+while [ $ciudad != "fin" ]
+do
+    ciudades[$cont]=$ciudad
+    read -p "escribe el nombre de ciudades termina con fin " ciudad
+    let cont=$cont+1
+done
+cont2=0
+while [ ${#ciudades[@]} -ge $cont2 ]
+do
+    echo ${ciudades[$cont2]}
+    let cont2=$cont2+1
+done
