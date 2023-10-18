8636abd258234b22f27b40a2fecc68c110817727
karto <karto@karto>
2023-10-18 12:06:17 +0200

    ejercicio 3

---
+++ practica7.3/3ejercicio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "nombre de usuario: " nombre
+
+nombreLenght=${#nombre}
+usuario=$( users | grep $nombre | cut -c 1-$nombreLenght usuario)
+
+if [[ $usuario = $nombre ]]
+    then
+        echo el usuario esta activo
+    else
+        echo el usuario no esta activo
+fi
