a3ec0f4c9d066972ca9cefc9adfccb6cd5ec7e3c
karto <karto@karto>
2023-10-18 12:24:07 +0200

    ejercicio 4

--- practica7.3/4ejercicio.sh
+++ practica7.3/4ejercicio.sh
@@ -2,5 +2,5 @@
 
 echo los usuario logeados son $(users)
 echo el nivel de ejecucion es 
-echo $(who | cut -c 1-11)
+echo $(who -r | cut -c 1-22)
 echo la fecha y hora de arranque del sistema es $(uptime -s)
