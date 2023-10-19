8c3f78c7ae80bda8a768dc4be908f4eacb0685a0
karto <karto@karto>
2023-10-19 13:07:12 +0200

    ejercicio 5 y colors

--- practica7.3/5ejercicio.sh
+++ practica7.3/5ejercicio.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-esNumero="^-?[0-9]+([-][0-9]+)?$"
+source colors
+esNumero="^[a-zA-Z]+$"
+nParametros=$#
+
+if [ $nParametros -ne 3 ]
+    then
+        echo -e "${RED}ERROR\n${CYAN}No se han introducido parametros"
+        exit
+fi
+
+if [[ $1 =~ $esNumero || $2 =~ $esNumero ]]
+    then
+        echo -e "${RED}ERROR\n${CYAN}No es un numero"
+        exit
+fi
+
+case $3 in
+    suma)
+        echo $(($1+$2))
+    ;;
+    resta)
+        echo $(($1-$2))
+    ;;
+    *)
+        echo -e "${RED}ERROR\n${CYAN}La operacion tiene que ser ${MAGENTA}(suma/resta)"
+    ;;
+esac
