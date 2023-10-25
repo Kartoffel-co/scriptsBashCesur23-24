ff1e296286676c7658372ce7c06d470bfc7fac6e
karto <karto@karto>
2023-10-25 11:47:08 +0200

    12

--- practica7.3/12ejercicio.sh
+++ practica7.3/12ejercicio.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 source colors
-No lo entiendo
+
+if [ -f ./random ]
+    then
+        rm random
+fi
+
+for i in {0..10000}
+    do
+        echo $(($RANDOM%1000)) >> random
+done
+
+gnuplot -e "set term png;set out 'random.png';set xlabel 'Numeros';set ylabel 'Frecuencia';plot 'random' with point"
